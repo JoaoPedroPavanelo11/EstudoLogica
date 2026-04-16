ac3fe09a2294131963e03be438c5647fa946499c
Joao Pedro <joaopedropavanelo@gmail.com>
2026-04-15 21:48:15 -0300

    primeiro commit

---
+++ src/LogicaLogin.java
@@ -0,0 +1,11 @@
+/*
+Senha correta: "1234"
+Usuário tem 3 tentativas
+Se acertar → "Acesso permitido"
+Se errar → "Senha incorreta"
+Se errar 3 vezes → "Conta bloqueada"
+ */
+
+
+public class LogicaLogin {
+}
